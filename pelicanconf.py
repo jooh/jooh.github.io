3236c8f77408e35b32fe4341593a00accbe141d4
Johan Carlin <johan.carlin@gmail.com>
2019-10-01 17:20:46 +0100

    fix ipynb format issues with custom css

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,7 +9,6 @@ SITEURL = ""
 DEFAULT_DATE_FORMAT = "%d %B %Y"
 USE_FOLDER_AS_CATEGORY = True
 DEFAULT_DATE = "fs"
-TYPOGRIFY = True
 THEME = "/Users/jc01/webdev/pelican-themes/pelican-bootstrap3"
 SHOW_ARTICLE_AUTHOR = True
 SHOW_DATE_MODIFIED = True
@@ -18,7 +17,7 @@ SHOW_DATE_MODIFIED = True
 # or BANNER
 BOOTSTRAP_THEME = "lumen"
 PLUGIN_PATHS = ["./plugins", "../pelican-plugins"]
-PLUGINS = ["pelican_gist", "ipynb.markup", "i18n_subsites"]
+PLUGINS = ["pelican_gist", "ipynb.markup", "i18n_subsites", "render_math"]
 MARKUP = ("md", "ipynb")
 PYGMENTS_STYLE = "borland"
 ABOUT_ME = "I work in human neuroimaging research at the University of Cambridge. I specialise in visual neuroscience, statistical methods, computational modelling, and data engineering. I'm a scientist, but I write a lot of code."
@@ -54,3 +53,19 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+# necessary to get ipynb to render properly
+# typogrify beaks mathjax
+TYPOGRIFY = False
+# IPYNB_FIX_CSS = True
+IPYNB_SKIP_CSS = False
+
+CUSTOM_CSS = 'custom.css'
+
+STATIC_PATHS = ["extra/CNAME", "extra/favicon.ico", "images", "extra/custom.css"]
+EXTRA_PATH_METADATA = {
+    "extra/CNAME": {"path": "CNAME"},
+    "extra/favicon.ico": {"path": "favicon.ico"},
+    "extra/custom.css": {"path": "custom.css"},
+}
+
--- publishconf.py
+++ publishconf.py
@@ -7,23 +7,20 @@ from __future__ import unicode_literals
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://www.johancarlin.com'
+SITEURL = "https://www.johancarlin.com"
 RELATIVE_URLS = False
 
-STATIC_PATHS = ['extra/CNAME','extra/favicon.ico','images']
-EXTRA_PATH_METADATA = {'extra/CNAME' : {'path': 'CNAME'},
-                       'extra/favicon.ico' : {'path': 'favicon.ico'}}
-
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
-DISQUS_SITENAME = 'johancarlin'
-GOOGLE_ANALYTICS = 'UA-33066815-1'
-TWITTER_USERNAME = 'johancarlin'
+DISQUS_SITENAME = "johancarlin"
+GOOGLE_ANALYTICS = "UA-33066815-1"
+TWITTER_USERNAME = "johancarlin"
